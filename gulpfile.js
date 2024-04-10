b96f243379806051e8571fefdcbfc89c3ebbe5b3
Lev Snegov <leffsnegov@yandex.ru>
2024-04-10 21:45:05 +0300

    подготовили к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
+}
 
 function html() {
   return gulp
@@ -13,10 +22,12 @@ function html() {
 }
 
 function css() {
+  const plugins = [];
   return gulp
     .src('src/blocks/**/*.css')
     .pipe(plumber())
     .pipe(concat('bundle.css'))
+    .pipe(postcss(plugins))
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({ stream: true }));
 }
@@ -39,13 +50,13 @@ function watchFiles() {
 }
 
 const build = gulp.series(clean, gulp.parallel(html, css, images));
-const watchapp = parallel(build, watchFiles, serve);
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
-exports.clean = clean;
-exports.images = images;
-exports.css = css;
 exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.clean = clean;
 
-exports.watchapp = watchapp;
 exports.build = build;
+exports.watchapp = watchapp;
 exports.default = watchapp;
