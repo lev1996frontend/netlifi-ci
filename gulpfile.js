97dd1820978938c3a4305de91bd3a10acf9592de
Lev Snegov <leffsnegov@yandex.ru>
2024-04-10 21:12:17 +0300

    подготовили к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,27 @@ const del = require('del');
 const browserSync = require('browser-sync').create();
 
 function html() {
-  return gulp.src('src/**/*.html')
-        .pipe(plumber())
-                .pipe(gulp.dest('dist/'))
-								.pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function css() {
-  return gulp.src('src/blocks/**/*.css')
-        .pipe(plumber())
-        .pipe(concat('bundle.css'))
-                .pipe(gulp.dest('dist/'))
-								.pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/blocks/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
-  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-            .pipe(gulp.dest('dist/images'))
-						.pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function clean() {
@@ -37,8 +40,6 @@ function watchFiles() {
 
 const build = gulp.series(clean, gulp.parallel(html, css, images));
 const watchapp = parallel(build, watchFiles, serve);
-
-
 
 exports.clean = clean;
 exports.images = images;
